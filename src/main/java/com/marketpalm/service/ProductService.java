041fa4beafe05f7f7527ad279e35d95641b4a178
HenryChagas <henry.wallacepaula14@gmail.com>
2026-05-16 12:25:51 -0300

    feat: ajustado salvamento de produto para carregar e exibir dados completos da categoria

--- src/main/java/com/marketpalm/service/ProductService.java
+++ src/main/java/com/marketpalm/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.marketpalm.service;
 
+import com.marketpalm.model.Category;
 import com.marketpalm.model.Product;
 import com.marketpalm.model.Sale;
+import com.marketpalm.repository.CategoryRepository;
 import com.marketpalm.repository.ProductRepository;
 import com.marketpalm.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +19,25 @@ public class ProductService {
     private SaleRepository saleRepository;
     @Autowired // O Spring injeta o repositório aqui automaticamente
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public Product salvarProduto(Product product) {
         // Regra Profissional: Validar se o preço é positivo
         if (product.getPrice().doubleValue() <= 0) {
             throw new RuntimeException("O preço deve ser maior que zero!");
         }
+
+        // NOVA LÓGICA: Se o produto veio com uma categoria e essa categoria tem um ID...
+        if (product.getCategory() != null && product.getCategory().getId() != null) {
+            // Buscamos a categoria completa no banco (que tem o nome preenchido)
+            Category category = categoryRepository.findById(product.getCategory().getId())
+                    .orElseThrow(() -> new RuntimeException("Categoria não encontrada!"));
+
+            // Associamos a categoria completa (com ID e Nome) ao produto antes de salvar
+            product.setCategory(category);
+        }
+
         return productRepository.save(product);
     }
 
